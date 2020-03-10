4e4240bdb887b2fb25d065b64f3a0f8a8d2540f1
NanoCE <jacob814@hotmail.com>
2020-03-10 17:44:58 -0400

    Update 1.1
    1.1 update

---
+++ Glass&Lights/v1.1/Glass+Lights/HarmonyPatches.cs
@@ -0,0 +1,31 @@
+﻿using Harmony;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace Glass_Lights
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyPatches
+    {
+        
+        static HarmonyPatches()
+        {
+            var harmony = HarmonyInstance.Create("Glass_Lights");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(PowerNetGraphics), "PrintWirePieceConnecting")]
+        static class Harmony_PrintWirePieceConnecting
+        {
+            static bool Prefix(SectionLayer layer, ref Thing A, Thing B, bool forPowerOverlay)
+            {
+                if (A.def.building != null && !A.def.building.allowWireConnection && A.TryGetComp<CompPowerTrader>() != null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
