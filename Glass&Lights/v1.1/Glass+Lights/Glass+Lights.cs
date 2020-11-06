cc467e5c634d5b834d5039033ccaac0bb7dad669
NanoCE <jacob814@hotmail.com>
2020-11-06 17:49:37 -0500

    Revert "Revert "Update 1.1""
    This reverts commit d8eaa62763e2620e3b62160332828868d5b9dbbc.

---
+++ Glass&Lights/v1.1/Glass+Lights/Glass+Lights.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+using Verse.AI;
+using static Glass_Lights.HarmonyPatches;
+
+namespace Glass_Lights
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            var harmony = HarmonyInstance.Create("Glass_Lights");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(SickPawnVisitUtility), "CanVisit")]
+        static class Harmony_FindRandomSickPawn
+        {
+            static bool Prefix(Pawn pawn, Pawn sick, JoyCategory maxPatientJoy, ref bool __result)
+            {
+                if(sick.needs.rest != null)
+                {                    
+                    return true;
+                }
+
+                Log.Message("Needs Null");
+                __result = sick.IsColonist && !sick.Dead && pawn != sick && sick.InBed() 
+                    && sick.Awake() && !sick.IsForbidden(pawn) && sick.needs.joy != null 
+                    && sick.needs.joy.CurCategory <= maxPatientJoy 
+                    && InteractionUtility.CanReceiveInteraction(sick) && !sick.needs.food.Starving
+                    && pawn.CanReserveAndReach(sick, PathEndMode.InteractionCell, Danger.None, 1, -1, null, false) 
+                    && !SickPawnVisitUtility.AboutToRecover(sick);
+                return false;
+            }
+        }        
+    }    
+}
