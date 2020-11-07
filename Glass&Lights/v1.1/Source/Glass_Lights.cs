39571f708aea0051719f77a92add9423b4eb08e7
NanoCE <jacob814@hotmail.com>
2020-11-07 11:03:41 -0500

    Cleanup
    Updating to make concurent with steam and future releases.

---
+++ Glass&Lights/v1.1/Source/Glass_Lights.cs
@@ -0,0 +1,85 @@
+﻿using Verse;
+
+namespace Glass_Lights
+{
+	public interface IRoofMounted
+	{
+		void Poof();
+	}
+
+	public interface IWallMounted
+	{
+		void Poof();
+	}
+
+	public class Building_OnCeiling : Building, IRoofMounted
+	{
+		public void Poof()
+		{
+			this.Destroy(DestroyMode.Refund);
+			//this.Kill(null, null);
+		}
+	}
+
+	public class Building_WallMounted : Building, IWallMounted
+	{
+		public void Poof()
+		{
+			this.Destroy(DestroyMode.Refund);
+			//if (!base.Position.Impassable(base.Map))
+			//{
+			//	this.Destroy(DestroyMode.Refund);
+			//}
+		}
+	}
+
+	public class PlaceWorker_LongWall : PlaceWorker
+	{
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			//loc is the empty space in front of the wall
+			//c is the offset tile that is being checked as a wall
+
+			//We want to check if the location that the player is placing the building is free and the second tile the building occupies is a wall. It is assumed the building is 1x2
+			//If both of these conditions are met, we return true, elsewise false.
+
+			AcceptanceReport result = false;
+			IntVec3 c = loc + rot.FacingCell; //offset tile
+
+			if(!loc.Impassable(map) && loc.InBounds(map))//Center tile is not impassible and in bounds
+			{
+				if (c.Impassable(map))//It is assumed that Impassable means a wall of some sort. This allows for placement on rocks and other larger buildings like geothermal generators if desired.
+				{
+					result = true;
+				}
+			}
+
+			//bool flag = c.Impassable(map);
+			//bool flag2 = !loc.Impassable(map) && loc.InBounds(map);
+			//bool flag3 = !flag && !flag2;
+			//if (!flag3)
+			//{
+			//	bool flag4 = !flag;
+			//	if (!flag4)
+			//	{
+			//		bool flag5 = !flag2;
+			//		if (!flag5)
+			//		{
+			//			result = true;
+			//		}
+			//	}
+			//}
+			return result;
+		}
+	}
+
+	public class PlaceWorker_UnderRoof : PlaceWorker
+	{
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			AcceptanceReport result = new AcceptanceReport("Ceiling mounted buildings must be placed under a roof that is unobstructed by a wall.");
+			result = map.roofGrid.Roofed(loc) && !loc.Impassable(map);
+			return result;
+		}
+	}
+}
---
+++ Glass&Lights/v1.1/Source/HarmonyPatch.cs
@@ -0,0 +1,100 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace Glass_Lights
+{
+	[StaticConstructorOnStartup]
+	internal static class HarmonyInit
+	{
+		static HarmonyInit()
+		{
+			var harmony = new Harmony("NanoCE.Glass_Lights");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[HarmonyPatch(typeof(PowerNetGraphics), "PrintWirePieceConnecting")]
+		static class Harmony_PrintWirePieceConnecting
+		{
+			static bool Prefix(SectionLayer layer, ref Thing A, Thing B, bool forPowerOverlay)
+			{
+				if (A.def.building != null && !A.def.building.allowWireConnection && A.TryGetComp<CompPowerTrader>() != null)
+				{
+					return false;
+				}
+				return true;
+			}
+		}
+
+		[HarmonyPatch(typeof(RoofGrid), "SetRoof")]
+		private static class Patch_SetRoof
+		{
+			private static void Postfix(Map ___map, ref IntVec3 c, ref RoofDef def)
+			{
+				if (def == null)
+				{
+					c.GetThingList(___map).OfType<Glass_Lights.IRoofMounted>().ToList<Glass_Lights.IRoofMounted>().ForEach(x =>
+					{
+						x.Poof();
+					});
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(Building), "Destroy")]
+		private static class Patch_BuildingDestroy
+		{
+			private static void Prefix(Building __instance)
+			{
+				if (__instance.def.passability == Traversability.Impassable)
+				{
+					//Log.Message(string.Format("{0} item is impassble, checking poof", __instance.def.defName));
+
+					(from m in __instance.Position.GetThingList(__instance.Map).OfType<Glass_Lights.IWallMounted>()
+					 where m != __instance
+					 select m).ToList<Glass_Lights.IWallMounted>().ForEach(x =>
+					 {
+						 //test
+						 //Log.Message("Wall Mounted thing found, poof");
+						 x.Poof();
+					 });
+				}
+			}
+		}
+
+		//Test Ignore Code
+
+		////Patchs for thingwithcomps that are not apparel that exist in recipes with apparel
+		//[HarmonyPatch(typeof(SpecialThingFilterWorker_NonDeadmansApparel), "Matches")]
+		//public static class Patch_SpecialThingFilterWorkerMatches
+		//{
+		//	//Defaults all ThingWithComp objects to return true if the special filter is called for Non-DeadmansApparel. Used for niche situations when
+		//	//an ThingWithComp is put in the Apparel Category which causes these items to appear in recipes that use the Apparel category tag. 
+		//	//ex: Recyling Apparel mods
+		//	public static void Postfix(ref bool __result, ref Thing t)
+		//	{
+		//		if(t != null && t is ThingWithComps && !(t is Apparel))
+		//		{
+		//			__result = true;
+		//		}
+		//	}
+		//}
+
+		//[HarmonyPatch(typeof(SpecialThingFilterWorker_DeadmansApparel), "Matches")]
+		//public static class Patch_SpecialThingFilterWorker_DeadmansApparel
+		//{
+		//	//Defaults all ThingWithComp objects to return true if the special filter is called for Deadmans Apparel. Used for niche situations when
+		//	//an ThingWithComp is put in the Apparel Category which causes these items to appear in recipes that use the Apparel category tag. 
+		//	//ex: Recyling Apparel mods
+		//	public static void Postfix(ref bool __result, ref Thing t)
+		//	{
+		//		if (t != null && t is ThingWithComps && !(t is Apparel))
+		//		{
+		//			__result = false;
+		//		}
+		//	}
+		//}
+	}
+}
